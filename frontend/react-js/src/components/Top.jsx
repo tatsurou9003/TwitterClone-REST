d720ca3f660968e2a7d4a8a2c7b196283189ff85
tatsurou9003 <t.murakami@4nonome.com>
2023-07-23 15:31:53 +0900

    top画面をいい感じにした

--- frontend/react-js/src/components/Top.jsx
+++ frontend/react-js/src/components/Top.jsx
@@ -1,9 +1,20 @@
 import React from "react";
 import { useNavigate } from "react-router-dom";
+import {
+  Flex,
+  Box,
+  Card,
+  CardHeader,
+  CardBody,
+  CardFooter,
+  Text,
+  Heading,
+  LinkBox,
+} from "@chakra-ui/react";
+import { ArrowForwardIcon } from "@chakra-ui/icons";
 
 export const Top = () => {
   const navigate = useNavigate();
-
   const handleLogin = () => {
     navigate("/login");
   };
@@ -13,9 +24,63 @@ export const Top = () => {
   };
 
   return (
-    <div>
-      <button onClick={handleLogin}>Login</button>
-      <button onClick={handleRegister}>Register</button>
-    </div>
+    <Flex
+      bg="teal.100"
+      justifyContent="center"
+      alignItems="center"
+      height="100vh"
+    >
+      <Box width="300px" margin="4">
+        <LinkBox
+          as="article"
+          borderWidth="1px"
+          rounded="lg"
+          onClick={handleRegister}
+        >
+          <Card
+            _hover={{
+              boxShadow: "lg",
+              backgroundColor: "gray.100",
+            }}
+          >
+            <CardHeader>
+              <Heading size="md">Sign up</Heading>
+            </CardHeader>
+            <CardBody>
+              <Text>一言呟いてかないか？</Text>
+            </CardBody>
+            <CardFooter>
+              <ArrowForwardIcon boxSize={6} />
+            </CardFooter>
+          </Card>
+        </LinkBox>
+      </Box>
+      <Box width="300px" margin="4">
+        <LinkBox
+          as="article"
+          maxW="sm"
+          borderWidth="1px"
+          rounded="lg"
+          onClick={handleLogin}
+        >
+          <Card
+            _hover={{
+              boxShadow: "lg",
+              backgroundColor: "gray.100",
+            }}
+          >
+            <CardHeader>
+              <Heading size="md">Login</Heading>
+            </CardHeader>
+            <CardBody>
+              <Text>元気そうで何よりだな。</Text>
+            </CardBody>
+            <CardFooter>
+              <ArrowForwardIcon boxSize={6} />
+            </CardFooter>
+          </Card>
+        </LinkBox>
+      </Box>
+    </Flex>
   );
 };
